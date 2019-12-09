099748ee2fe9dbf15d31b048fb8f149d0aeb456b
Go Gonzo <dawid.kaledkowski@gmail.com>
2019-12-09 19:53:48 +0100

    remove unsigned int from checks

--- src/checks.h
+++ src/checks.h
@@ -13,7 +13,7 @@ namespace checks {
         Rcpp::stop("k can't be negative");
       }
     } else if (k.size() > 1) {
-      for (unsigned int i = 0; i < n; i++) {
+      for (int i = 0; i < n; i++) {
         if (k(i) < 0) {
           Rcpp::stop("k can't be negative");
         }
@@ -29,7 +29,7 @@ namespace checks {
     }
 
     if (idx.size() > 1) {
-      for (unsigned int i = 1; i < n; i++) {
+      for (int i = 1; i < n; i++) {
         if (idx(i) < idx(i - 1)) {
           Rcpp::stop("idx have to be in descending order");
         }
